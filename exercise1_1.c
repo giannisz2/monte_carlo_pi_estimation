12d1002d0f74801a54ab6b52266a2036fc3c3f71
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-07-01 18:59:54 +0300

    Minor security fixes

--- exercise1_1.c
+++ exercise1_1.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <unistd.h> // Needed for close()
 #include "my_rand.h"
 
 pthread_mutex_t mutex;
@@ -15,21 +17,20 @@ typedef struct {
 } ThreadArguments;
 
 void* piEstimation(void* args) {
-    
     ThreadArguments *thread_args = (ThreadArguments*)args;
-    unsigned seed = time(NULL);
+    unsigned int seed = rand();
 
-    int local_m = thread_args->throws/thread_args->num_threads;
+    int local_m = thread_args->throws / thread_args->num_threads;
     int first_i = local_m * thread_args->current_thread;
     int last_i = first_i + local_m;
 
     int local_hits = 0;
-    for(int i = first_i; i < last_i; i++) {
-        double x =  my_drand(&seed) * 2 - 1;
-        double y =  my_drand(&seed) * 2 - 1;
-        double sq_distance = x*x + y*y;
-        if(sq_distance <= 1)  
-            local_hits++;         
+    for (int i = first_i; i < last_i; i++) {
+        double x = my_drand(&seed) * 2 - 1;
+        double y = my_drand(&seed) * 2 - 1;
+        double sq_distance = x * x + y * y;
+        if (sq_distance <= 1)
+            local_hits++;
     }
 
     pthread_mutex_lock(&mutex);
@@ -37,52 +38,56 @@ void* piEstimation(void* args) {
     pthread_mutex_unlock(&mutex);
 
     return NULL;
-
 }
 
 int main(int argc, char* argv[]) {
-
-    if(argc < 2) {
-        perror("Give 2 arguments when you execute the program.\n");
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <throws> <num_threads>\n", argv[0]);
         return 1;
     }
 
-    FILE* fp = fopen("data1_1.txt", "w");
-    if(fp == NULL) {
-        perror("File couldn't open.\n");
+    long long throws = strtol(argv[1], NULL, 10);
+    int num_threads = strtol(argv[2], NULL, 10);
+
+    if (errno == ERANGE) {
+        perror("Conversion error: Number out of range.\n");
+        return 1;
+    }
+
+    printf("We will throw %lld darts.\n\n", throws);
+
+    int fd = open("data1_1.txt", O_WRONLY | O_CREAT | O_EXCL, 0644);
+    if (fd == -1) {
+        perror("File couldn't be opened or created.\n");
         return 2;
     }
 
-    ThreadArguments *thread_args = malloc(sizeof(ThreadArguments));
-
-    thread_args->throws = atoi(argv[1]);
-    thread_args->num_threads = atoi(argv[2]);
-
-    long long int throws = thread_args->throws;
-    int num_threads = thread_args->num_threads;
-    unsigned seed = time(NULL);
-
-
-    printf("We will throw %lld darts.\n\n", throws);
+    FILE* fp = fdopen(fd, "w");
+    if (fp == NULL) {
+        perror("File stream couldn't be created.\n");
+        close(fd);  // Close the file descriptor on failure
+        return 2;
+    }
 
     printf("We will start with sequential code.\n");
 
     clock_gettime(CLOCK_MONOTONIC, &start1);
 
     long long int hits = 0;
-    for(int i = 0; i < throws; i++) {
-        double x =  my_drand(&seed) * 2 - 1;
-        double y =  my_drand(&seed) * 2 - 1;
-        double sq_distance = x*x + y*y;
-        if(sq_distance <= 1)
+    unsigned seed = time(NULL);
+
+    for (int i = 0; i < throws; i++) {
+        double x = my_drand(&seed) * 2 - 1;
+        double y = my_drand(&seed) * 2 - 1;
+        double sq_distance = x * x + y * y;
+        if (sq_distance <= 1)
             hits++;
     }
 
     clock_gettime(CLOCK_MONOTONIC, &end1);
-    
-    double elapsed_time = (end1.tv_sec - start1.tv_sec) + (end1.tv_nsec - start1.tv_nsec) / 1e9;
 
-    double pi_estimation = 4*(hits/(double)throws);
+    double elapsed_time = (end1.tv_sec - start1.tv_sec) + (end1.tv_nsec - start1.tv_nsec) / 1e9;
+    double pi_estimation = 4 * (hits / (double)throws);
 
     printf("The estimation of pi is %lf and it took %lf seconds.\n\n", pi_estimation, elapsed_time);
 
@@ -94,33 +99,35 @@ int main(int argc, char* argv[]) {
     pthread_mutex_init(&mutex, NULL);
 
     clock_gettime(CLOCK_MONOTONIC, &start2);
-    for(int i = 0; i < num_threads; i++) {
-        thread_args->current_thread = i;
-        if(pthread_create(&threads[i], NULL, &piEstimation, (void*)thread_args) != 0) {
+    ThreadArguments thread_args[num_threads]; // Create an array for thread arguments
+
+    for (int i = 0; i < num_threads; i++) {
+        thread_args[i].throws = throws;
+        thread_args[i].num_threads = num_threads;
+        thread_args[i].current_thread = i;
+        if (pthread_create(&threads[i], NULL, piEstimation, (void*)&thread_args[i]) != 0) {
             perror("Something went wrong with creating the threads.\n");
             return 3;
         }
     }
 
-    for(int i = 0; i < num_threads; i++) {
-        if(pthread_join(threads[i], NULL) != 0) {
+    for (int i = 0; i < num_threads; i++) {
+        if (pthread_join(threads[i], NULL) != 0) {
             perror("Something went wrong with joining.\n");
             return 4;
         }
     }
-
 
     clock_gettime(CLOCK_MONOTONIC, &end2);
     elapsed_time = (end2.tv_sec - start2.tv_sec) + (end2.tv_nsec - start2.tv_nsec) / 1e9;
 
     fprintf(fp, "%lld %d %lf\n", throws, num_threads, elapsed_time);
 
-    pi_estimation = 4*(total_hits/(double)throws);
+    pi_estimation = 4 * (total_hits / (double)throws);
 
     printf("The estimation of pi is %lf and it took %.6f seconds.\n\n", pi_estimation, elapsed_time);
 
     pthread_mutex_destroy(&mutex);
-    free(thread_args);
     fclose(fp);
 
     return 0;
